5b9059a3892ac1163042291175e74848331cc0c3
johnodhiambo0 <johneliud4@gmail.com>
2024-11-07 09:18:23 +0300

    add: LookupIdentifier function to help in distinguishing keywords encountered in the source code from other identifier values.

--- internal/token/tokens.go
+++ internal/token/tokens.go
@@ -47,3 +47,22 @@ const (
 	ELSE     = "ELSE"
 	RETURN   = "RETURN"
 )
+// Map keywords in the language to their respective token types.
+var keywords = map[string]string{
+	"func":   FUNCTION,
+	"var":    VAR,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+// Check if a given identifier is a keyword and return the corresponding type. Returns the IDENTIFIER type if not a keyword.
+func LookupIdentifier(identifier string) string {
+	tok, ok := keywords[identifier]
+	if ok {
+		return tok
+	}
+	return IDENTIFIER
+}
