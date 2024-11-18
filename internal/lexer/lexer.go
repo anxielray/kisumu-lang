99190c0ac4e45f009941a79af22f905aa17307f1
John <johneliud4@gmail.com>
2024-11-18 19:52:27 +0300

    chore: create a struct that will represent a lexer implementation. define function New that will create a new instance of a lexer struct.

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,1 +1,17 @@
 package lexer
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	char         byte
+}
+
+// Will be used to create a new Lexer instance
+func New(input string) *Lexer {
+	l := &Lexer{
+		input: input,
+	}
+	l.readCurrentCharacter()
+	return l
+}
