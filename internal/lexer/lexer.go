d812b63c915d0edfa54b168b12078cc8e57bceb0
John <johneliud4@gmail.com>
2024-11-18 21:00:29 +0300

    add: function NextToken that reads the next token from the input.

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -83,11 +83,91 @@ func (l *Lexer) skipWhiteSpace() {
 	}
 }
 
-// Will check the next character in the input string without advancing the read position. 
+// Will check the next character in the input string without advancing the read position.
 func (l *Lexer) peekNextCharacter() byte {
 	if l.readPosition >= len(l.input) {
 		return 0
 	} else {
 		return l.input[l.readPosition]
 	}
+}
+
+// Will read the next token from the input source
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhiteSpace()
+
+	switch l.char {
+	// Checks whether the next token is either EQUAL (==) or ASSIGN (=)
+	case '=':
+		if l.peekNextCharacter() == '=' {
+			ch := l.char
+			l.readCurrentCharacter()
+			literal := string(ch) + string(l.char)
+			tok = token.Token{Type: token.EQUAL, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.char)
+		}
+	case '+':
+		tok = newToken(token.PLUS, l.char)
+	case '-':
+		tok = newToken(token.MINUS, l.char)
+		// Checks whether the next token is either NOTEQUAL (!=) or NOT (!)
+	case '!':
+		if l.peekNextCharacter() == '=' {
+			ch := l.char
+			l.readCurrentCharacter()
+			literal := string(ch) + string(l.char)
+			tok = token.Token{Type: token.NOTEQUAL, Literal: literal}
+		} else {
+			tok = newToken(token.NOT, l.char)
+		}
+	case '*':
+		tok = newToken(token.ASTERIK, l.char)
+	case '/':
+		tok = newToken(token.SLASH, l.char)
+	case '<':
+		tok = newToken(token.LESSTHAN, l.char)
+	case '>':
+		tok = newToken(token.GREATERTHAN, l.char)
+	case ',':
+		tok = newToken(token.COMMA, l.char)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.char)
+	case '(':
+		tok = newToken(token.LEFTPARENTHESIS, l.char)
+	case ')':
+		tok = newToken(token.RIGHTPARENTHESIS, l.char)
+	case '{':
+		tok = newToken(token.LEFTBRACE, l.char)
+	case '}':
+		tok = newToken(token.RIGHTBRACE, l.char)
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
+	case '[':
+		tok = newToken(token.LEFTBRACKET, l.char)
+	case ']':
+		tok = newToken(token.RIGHTBRACKET, l.char)
+	case ':':
+		tok = newToken(token.COLON, l.char)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.char) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdentifier(tok.Literal)
+			return tok
+		} else if isDigit(l.char) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.char)
+		}
+	}
+	l.readCurrentCharacter()
+	return tok
 }
