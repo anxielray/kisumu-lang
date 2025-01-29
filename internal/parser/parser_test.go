3dbac14d188e9b5442ef969271750cc7ed3e61da
khahussein <kherld.hussein@gmail.com>
2025-01-29 04:55:18 +0300

    refactor(internal): reformat code with gofmt -s -l -w .

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,29 +1,28 @@
 package parser
 
 import (
- "testing"
- "github.com/Zone01-Kisumu-Open-Source-Projects/kisumu-lang/internal/lexer"
- "github.com/Zone01-Kisumu-Open-Source-Projects/kisumu-lang/internal/ast"
+	"github.com/Zone01-Kisumu-Open-Source-Projects/kisumu-lang/internal/ast"
+	"github.com/Zone01-Kisumu-Open-Source-Projects/kisumu-lang/internal/lexer"
+	"testing"
 )
 
-
 func TestParseIntNode(t *testing.T) {
-    input := "42"
-    lexer := lexer.New(input, lexer.DefaultConfig) // Ensure this is your Lexer
-    parser := New(lexer)         // Update to match `New` for `Parser`
+	input := "42"
+	lexer := lexer.New(input, lexer.DefaultConfig) // Ensure this is your Lexer
+	parser := New(lexer)                           // Update to match `New` for `Parser`
 
-    node := parser.parseIntLiteral()
+	node := parser.parseIntLiteral()
 
-    if node == nil {
-        t.Fatalf("parseIntLiteral returned nil")
-    }
+	if node == nil {
+		t.Fatalf("parseIntLiteral returned nil")
+	}
 
-    intNode, ok := node.(*ast.IntNode)
-    if !ok {
-        t.Fatalf("expected *ast.IntNode, got %T", node)
-    }
+	intNode, ok := node.(*ast.IntNode)
+	if !ok {
+		t.Fatalf("expected *ast.IntNode, got %T", node)
+	}
 
-    if intNode.Value != 42 {
-        t.Errorf("expected value 42, got %d", intNode.Value)
-    }
+	if intNode.Value != 42 {
+		t.Errorf("expected value 42, got %d", intNode.Value)
+	}
 }
